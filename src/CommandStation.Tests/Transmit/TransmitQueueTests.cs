8b4974c605b42c48e6267741ef9c2cf5d2f1f5c1
Lars Møllebjerg <lmoellebjerg@sdl.com>
2018-01-27 11:19:13 +0100

    Add needed unit test nuget packages and set up a dummy test.

---
+++ src/CommandStation.Tests/Transmit/TransmitQueueTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Trainiot.CommandStation.Transmit.Tests
+{
+    public class TransmitQueueTests
+    {
+        [Fact]
+        public void MakeTestsHere()
+        {
+            Assert.True(true);
+        }
+    }
+}
